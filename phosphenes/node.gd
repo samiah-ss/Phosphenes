2a03e8542edaa8c4ed6557906ee933f0995b17a2
Samiah <samsid813@gmail.com>
2026-07-30 22:20:28 -0700

    Comments

--- phosphenes/node.gd
+++ phosphenes/node.gd
@@ -1,15 +1,18 @@
 extends Node
 
+# Set different labels to variables.
 @onready var label1 = $"/root/Sprite2D/GameManager/onestartext"
 @onready var label2 = $"/root/Sprite2D/GameManager/twostartext"
 @onready var label3 = $"/root/Sprite2D/GameManager/threestartext"
 
-var stars_found = 0
+var stars_found = 0 # Initialize a lack of found stars.
 
+# Update levels once star count increases.
 func star_found():
 	stars_found+=1
 	update_labels()
 
+# Update labels by turning them black according to the total amount of found stars.
 func update_labels():
 	if stars_found >= 1:
 		label1.modulate = Color.BLACK
