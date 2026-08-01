2055ac4774ad59b068dfefee721fcf81533ecbc1
Samiah <samsid813@gmail.com>
2026-07-31 21:50:26 -0700

    Final Touches
    - Increased Readability Periods by One Second

--- phosphenes/star_2.gd
+++ phosphenes/star_2.gd
@@ -9,7 +9,7 @@ func _pressed():
 	get_parent().star_found() # Get and run the star_found function from the parent object.
 	var tween: Tween = create_tween() # Create a new tween effect.
 	tween.tween_property(vl2, "modulate:a", 1.0, 0.5) # Incorporate a 0.5-second fade-in.
-	tween.tween_interval(2.0) # Pause tweening effects for a 2-second readability window.
+	tween.tween_interval(3.0) # Pause tweening effects for a 2-second readability window.
 	tween.tween_property(vl2, "modulate:a", 0.0, 0.5) # Incorporate a 0.5-second fade-out.
 	await tween.finished # Mark tweening as finished.
 	vl2.visible = false # Undo dialogue visibility.
